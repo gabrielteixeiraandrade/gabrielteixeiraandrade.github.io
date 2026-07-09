7f8dc20cdce89b16a0d9d6556f3d3647c2a0b27a
Gabriel Teixeira Andrade <gabrielteixa800@gmail.com>
2026-07-09 20:25:19 -0300

    Organized the files and turn the Scanner application the main page

---
+++ docs/js/produtos.js
@@ -0,0 +1,13 @@
+const bancoDeDadosProdutos = {
+
+    "1234567890123": {
+        nome: "Produto de Teste",
+        pagina: "produtos/teste.html"
+    },
+
+    "7891152801842": {
+        nome: "Biscoito Recheado Richester Escureto 125g",
+        pagina: "produtos/teste.html"
+    }
+
+};
